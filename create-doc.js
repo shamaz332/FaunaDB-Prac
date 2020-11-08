f3418e3d1d07bf59f87ef12a252f240c628e92c3
shamaz332 <shamazsaeed332@gmail.com>
2020-11-08 19:51:01 +0500

    collection field data added

---
+++ create-doc.js
@@ -0,0 +1,33 @@
+  
+    const faunadb = require('faunadb'),
+    q = faunadb.query;
+  require('dotenv').config();
+  
+  
+  (async () =>{
+  
+      var client = new faunadb.Client({ secret: "fnAD6IskqrACBZfggJzQvKo6IhakeKuyCpDUAmY2"});
+      
+      //create database
+      try {
+        var result = await client.query(
+          q.Create(
+            q.Collection('firstCollection'),
+            { data: { title: 'Serverless applications Database' } },
+          )
+        );
+        console.log(result);
+      }  
+      catch (error){
+        if (error.requestResult.statusCode === 400 && error.message === 'instance already exists') {
+          console.log('Database with this name already exists');
+        }
+        else {
+          console.log('Unknow Error: ');
+          console.log(error);
+        }
+        
+      }
+  
+  })();
+  
