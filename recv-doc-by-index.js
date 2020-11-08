eb652ad03c6bdc46facfcb18545fe77bae7f49a4
shamaz332 <shamazsaeed332@gmail.com>
2020-11-08 21:28:31 +0500

    recv doc with index

---
+++ recv-doc-by-index.js
@@ -0,0 +1,31 @@
+const faunadb = require("faunadb"),
+  q = faunadb.query;
+require("dotenv").config();
+
+(async () => {
+  var client = new faunadb.Client({
+    secret: "fnAD6IskqrACBZfggJzQvKo6IhakeKuyCpDUAmY2",
+  });
+
+  //create database
+  try {
+    var result = await client.query(
+      
+        q.Get(
+            q.Match(q.Index('post_title'), 'Third Title')
+          )
+
+    );
+    console.log(result);
+  } catch (error) {
+    if (
+      error.requestResult.statusCode === 400 &&
+      error.message === "instance already exists"
+    ) {
+      console.log("Database with this name already exists");
+    } else {
+      console.log("Unknow Error: ");
+      console.log(error);
+    }
+  }
+})();
